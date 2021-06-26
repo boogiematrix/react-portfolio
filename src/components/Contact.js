e223f25abe41ab53797bc7751716e36ed0b3ae32
Night Chough <chicken.snuggles@gmail.com>
2021-06-26 17:40:27 -0500

    added contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export function Contact() {
-    return (
+export function Contact(props) {
+    const [contactName, setContactName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        if (name === contactName) {
+            setContactName(value)
+        } else if (name === email) {
+            setEmail(value)
+        } else {
+            setMessage(value)
+        }
+
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
         
+    }
+
+    let alertMessage = ''
+
+    return (
+        <div>
+            <form>
+                <input
+                    value={contactName}
+                    name="contactName"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Name"
+                    onBlur={alertMessage = 'This field is required'}
+                />
+                <input
+                    value={email}
+                    name="email"
+                    onChange={handleInputChange}
+                    type="email"
+                    placeholder="email"
+                    onBlur={alertMessage = 'This field is required'}
+                />
+                <input
+                    value={message}
+                    name="message"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="message..."
+                    onBlur={alertMessage = 'This field is required'}
+                />
+                <button type="button" onClick={handleFormSubmit}>
+                    Submit
+                </button>
+            </form>
+            <h3>{alertMessage}</h3>
+        </div>
     )
 }
