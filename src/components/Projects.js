36170f37c79fba33f005ec88bdb95f67cf190017
Night Chough <chicken.snuggles@gmail.com>
2021-06-26 16:43:21 -0500

    created and array of project data

---
+++ src/components/Projects.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const projectData = [
+    {
+        title: 'Fitness Tracker',
+        repoName: 'workout-tracker',
+        deployedName: 'https://blooming-scrubland-28026.herokuapp.com/',
+        image: 'https://github.com/boogiematrix/workout-tracker/blob/main/public/images/workout-tracker-screenshot.png?raw=true',
+    },
+    {
+        title: 'Court Finder',
+        repoName: 'court-finder',
+        deployedName: 'https://quiet-beyond-45478.herokuapp.com/',
+        image: 'https://github.com/boogiematrix/court-finder/blob/main/Website.png?raw=true',
+    },
+    {
+        title: 'Gotta Catch A Vibe',
+        repoName: 'Gotta-Catch-A-Vibe',
+        deployedName: 'https://boogiematrix.github.io/Gotta-Catch-A-Vibe/',
+        image: 'https://github.com/boogiematrix/Gotta-Catch-A-Vibe/blob/main/public/images/pokemonHeader.png?raw=true',
+    },
+    {
+        title: 'Budget Tracker',
+        repoName: 'pwa-budget-tracker',
+        deployedName: 'https://cryptic-temple-61605.herokuapp.com/',
+        image: 'https://github.com/boogiematrix/pwa-budget-tracker/blob/main/public/images/budget-tracker-screenshot.png?raw=true',
+    },
+    {
+        title: 'Note Taker',
+        repoName: 'note-taker',
+        deployedName: 'https://enigmatic-depths-78679.herokuapp.com/',
+        image: 'https://github.com/boogiematrix/note-taker/blob/main/public/assets/pictures/notes-screenshot.png?raw=true',
+    },
+    {
+        title: 'Code Quiz',
+        repoName: 'code-quiz',
+        deployedName: 'https://boogiematrix.github.io/code-quiz/',
+        image: 'https://raw.githubusercontent.com/boogiematrix/code-quiz/main/Screen%20Shot%202021-03-05%20at%208.01.48%20PM.png',
+    }
+];
